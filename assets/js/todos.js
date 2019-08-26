fa182833056aa49437d0de9ee93a93c87c9c021e
Melissa Harris <53122980+Melissa-Harris-umich@users.noreply.github.com>
2019-08-26 15:39:53 -0400

    Added event listener to span with fade

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,3 +3,12 @@ $("li").click(function() {
     $(this).toggleClass("completed")
 });
 
+//When span is clicked, todo will fade out
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+
+    });
+    event.stopPropagation();
+});
+
